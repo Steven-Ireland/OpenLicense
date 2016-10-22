e2e6110ac5ec905334e31158592890dc54438fa1
Steven Ireland <scientastersteve@gmail.com>
2016-10-22 15:14:02 -0400

    tiny bit of unit testing to try out mocha

---
+++ test/schema/TestProduct.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var Product = require('../../dist/schema/Product');
+
+describe('Product', function() {
+	describe('#generateSecret', function() {
+		it('should generate a valid unused secret', function() {
+			var product = new Product();
+			product.generateSecret(function() {
+				assert(product.secret);
+			});
+		});
+		it('should be 128 length', function() {
+			var product = new Product();
+			product.generateSecret(function() {
+				assert(product.secret.length == 128);
+			});
+		});
+	});
+});
